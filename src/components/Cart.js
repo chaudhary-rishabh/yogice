ac52b9debf2bf807f27475b0b29064a7b6c2fce8
Rishabh <chaudharyrishabh029@gmail.com>
2025-01-14 11:42:29 +0530

    removed google pay from cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useCart } from "./CartContext";
-import "@google-pay/button-react";
+// import "@google-pay/button-react";
 import "../style/Common.css";
 import OrderForm from "./OrderForm"
 
@@ -9,86 +9,86 @@ const Cart = () => {
   const totalPrice = cartItems.reduce((total, item) => total + item.price, 0).toFixed(2);
   const orderFormRef = useRef(null);
   // Define your Google Pay payment request
-  const paymentRequest = {
-    apiVersion: 2,
-    apiVersionMinor: 0,
-    allowedPaymentMethods: [
-      {
-        type: "CARD",
-        parameters: {
-          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-          allowedCardNetworks: ["AMEX", "DISCOVER", "JCB", "MASTERCARD", "VISA"],
-        },
-        tokenizationSpecification: {
-          type: "PAYMENT_GATEWAY",
-          parameters: {
-            gateway: "example", // Replace with your payment gateway name
-            gatewayMerchantId: "BCR2DN4TW2ZKHZAT", // Replace with your gateway merchant ID
-          },
-        },
-      },
-    ],
-    merchantInfo: {
-      merchantId: "BCR2DN4TW2ZKHZAT", // Replace with your merchant ID
-      merchantName: "yogice", // Replace with your merchant name
-    },
-    transactionInfo: {
-      totalPriceStatus: "FINAL",
-      totalPrice: (cartItems.reduce((total, item) => total + item.price, 0)).toFixed(2), // Calculate the total price of items in the cart
-      currencyCode: "INR", // Change to "INR" for Indian Rupees or your desired currency code
-    },
-  };
+  // const paymentRequest = {
+  //   apiVersion: 2,
+  //   apiVersionMinor: 0,
+  //   allowedPaymentMethods: [
+  //     {
+  //       type: "CARD",
+  //       parameters: {
+  //         allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+  //         allowedCardNetworks: ["AMEX", "DISCOVER", "JCB", "MASTERCARD", "VISA"],
+  //       },
+  //       tokenizationSpecification: {
+  //         type: "PAYMENT_GATEWAY",
+  //         parameters: {
+  //           gateway: "example", // Replace with your payment gateway name
+  //           gatewayMerchantId: "BCR2DN4TW2ZKHZAT", // Replace with your gateway merchant ID
+  //         },
+  //       },
+  //     },
+  //   ],
+  //   merchantInfo: {
+  //     merchantId: "BCR2DN4TW2ZKHZAT", // Replace with your merchant ID
+  //     merchantName: "yogice", // Replace with your merchant name
+  //   },
+  //   transactionInfo: {
+  //     totalPriceStatus: "FINAL",
+  //     totalPrice: (cartItems.reduce((total, item) => total + item.price, 0)).toFixed(2), // Calculate the total price of items in the cart
+  //     currencyCode: "INR", // Change to "INR" for Indian Rupees or your desired currency code
+  //   },
+  // };
 
   // Function to handle Google Pay button click
-  const handleGooglePayClick = () => {
-    const googlePay = new window.google.payments.api.PaymentsClient({ environment: "TEST" });
- // Use "TEST" or "PRODUCTION"
+//   const handleGooglePayClick = () => {
+//     const googlePay = new window.google.payments.api.PaymentsClient({ environment: "TEST" });
+//  // Use "TEST" or "PRODUCTION"
 
-    googlePay
-      .loadPaymentData(paymentRequest)
-      .then((paymentData) => {
-        // Handle the payment data here
-      })
-      .catch((error) => {
-        // Handle errors here
-        console.error(error);
-      });
-  };
+//     googlePay
+//       .loadPaymentData(paymentRequest)
+//       .then((paymentData) => {
+//         // Handle the payment data here
+//       })
+//       .catch((error) => {
+//         // Handle errors here
+//         console.error(error);
+//       });
+//   };
 
   // Call the initialization function when your component mounts
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://pay.google.com/gp/p/js/pay.js";
-    script.async = true;
-    script.onload = () => {
-      // Initialize Google Pay when the script is loaded
-      initializeGooglePay();
-    };
-    document.head.appendChild(script);
-  }, []);
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "https://pay.google.com/gp/p/js/pay.js";
+  //   script.async = true;
+  //   script.onload = () => {
+  //     // Initialize Google Pay when the script is loaded
+  //     initializeGooglePay();
+  //   };
+  //   document.head.appendChild(script);
+  // }, []);
 
-  const initializeGooglePay = () => {
-    // Check if the `google` object is defined
-    if (window.google) {
-      const googlePay = new window.google.payments.api.PaymentsClient({
-        environment: "TEST", // Use "TEST" or "PRODUCTION"
-      });
+  // const initializeGooglePay = () => {
+  //   // Check if the `google` object is defined
+  //   if (window.google) {
+  //     const googlePay = new window.google.payments.api.PaymentsClient({
+  //       environment: "TEST", // Use "TEST" or "PRODUCTION"
+  //     });
 
-      googlePay
-        .isReadyToPay({ ...paymentRequest }) // Check if Google Pay is ready
-        .then((response) => {
-          if (response.result) {
-            // Google Pay is ready; you can use it here
-          } else {
-            // Google Pay is not available for the user's device or browser
-          }
-        })
-        .catch((error) => {
-          // Handle initialization errors here
-          console.error(error);
-        });
-    }
-  };
+  //     googlePay
+  //       .isReadyToPay({ ...paymentRequest }) // Check if Google Pay is ready
+  //       .then((response) => {
+  //         if (response.result) {
+  //           // Google Pay is ready; you can use it here
+  //         } else {
+  //           // Google Pay is not available for the user's device or browser
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         // Handle initialization errors here
+  //         console.error(error);
+  //       });
+  //   }
+  // };
 
     const [showOrderForm, setShowOrderForm] = useState(false);
   useEffect(() => {
